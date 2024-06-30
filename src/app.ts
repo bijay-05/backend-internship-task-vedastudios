46365f86446550519ccc1c885754d85b04fffcde
bijay-05 <hawkeyesnipert69@gmail.com>
2024-06-30 16:06:06 +0545

    feat: added Image in Product resource

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import express from 'express';
 import AdminJSExpress from '@adminjs/express';
 import { Database, Resource, getModelByName } from '@adminjs/prisma';
 
+import { Components, componentLoader } from './components.js';
 
 const prisma = new PrismaClient();
 
@@ -19,10 +20,62 @@ const start = async () => {
     },
     {
       resource: { model: getModelByName('Product'), client: prisma },
-      options: {}
+      options: {
+        properties: {
+          Image: {
+            type: 'string',
+            components: {
+              list: Components.imageList,
+              show: Components.imageShow,
+            }
+          }
+        },
+        actions: {
+          list: {  // using `after` hook to modify the response from client
+                    // and adding image properties ( from Image table ) for the product
+              after: async (response) => {
+                  const products = response.records.map(async (record) => {
+                      const image = await prisma.image.findFirst({
+                          where: { productId: record.id },
+                          orderBy: {
+                              id: 'desc',
+                          },
+                          select: {
+                            'url': true,
+                            'id': true
+                          }
+                      });
+                      record.params.Image = image;
+                      return record;
+                  });
+                  response.records = await Promise.all(products);
+                  return response;
+              }
+          },
+          show: {
+              after: async (response) => {
+                  const record = response.record;
+                  const image = await prisma.image.findFirst({
+                      where: {productId: record.id},
+                      orderBy: {
+                        id: 'desc',
+                      },
+                      select: {
+                        'url': true
+                      }
+                  });
+                  response.record.params.Image = image;
+
+                  return response;
+              }
+          }
+
+        }
+      }
     }
-  ]
-  }
+  ],
+  componentLoader
+}
 
   const app = express();
 
