7b600591ba4acd4dd8006c3a8b37af9531377e6d
bijay-05 <hawkeyesnipert69@gmail.com>
2024-06-30 16:33:24 +0545

    feat: organized syntax

--- src/app.ts
+++ src/app.ts
@@ -88,7 +88,7 @@ const start = async () => {
   app.use(admin.options.rootPath, adminRouter);
 
   app.listen(PORT, () => {
-    console.log(`AdminJS started on http:20.244.35.164:${PORT}${admin.options.rootPath}`)
+    console.log(`AdminJS started on localhost:${PORT}${admin.options.rootPath}`)
   })
 }
 
